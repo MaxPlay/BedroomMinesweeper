c87c8164004e0317d0b178a56860be832921388b
MaxPlay <maxplayyt@gmail.com>
2019-05-05 21:15:49 +0200

    Added ECS stub

---
+++ Bedroom.Minesweeper/ECS/Components/Component.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bedroom.Minesweeper.ECS.Components
+{
+    public abstract class Component
+    {
+        private Entity entity;
+
+        public Component(Entity entity)
+        {
+            this.entity = entity;
+        }
+
+        public Entity Entity
+        {
+            get
+            {
+                return entity;
+            }
+
+            set
+            {
+                if (value == null)
+                    return;
+
+                if (entity == value)
+                    return;
+
+                entity.RemoveComponent(this);
+                entity = value;
+                entity.AddComponent(this);
+            }
+        }
+    }
+}
---
+++ Bedroom.Minesweeper/ECS/Entity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bedroom.Minesweeper.ECS.Components;
+
+namespace Bedroom.Minesweeper.ECS
+{
+    public class Entity
+    {
+        public string Name { get; set; }
+
+        List<Component> components;
+
+        public bool RemoveComponent(Component component)
+        {
+            return components.Remove(component);
+        }
+
+        public void AddComponent(Component component)
+        {
+            components.Add(component);
+        }
+
+        public T AddComponent<T>() where T : Component
+        {
+            T component = Activator.CreateInstance(typeof(T), new object[] { this }) as T;
+            components.Add(component);
+            return component;
+        }
+
+        public T GetComponent<T>() where T : Component
+        {
+            return components.FirstOrDefault() as T;
+        }
+
+        public IEnumerable<T> GetComponents<T>() where T : Component
+        {
+            List<T> result = new List<T>(components.Count);
+            for (int i = 0; i < components.Count; i++)
+            {
+                if (components[i] is T)
+                    result.Add((T)components[i]);
+            }
+            return result;
+        }
+    }
+}
